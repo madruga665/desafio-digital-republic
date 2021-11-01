cdd0be11fb52b5e8cbc08704db67ddb412f1e5b7
Luciano Amâncio <madruga665@hotmail.com>
2021-11-01 11:56:06 -0300

    feat: add todos os componentes Form na home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,8 +4,17 @@ import styles from './styles.module.scss'
 
 export default function Home() {
   return (
-    <div className={styles.homeContainer}>
-      <Form title='Parede 1'/>
+    <div className={styles.homeWrapper}>
+      <div className={styles.formContainer}>
+        <Form title='Parede 1'/>
+        <Form title='Parede 2'/>
+        <Form title='Parede 3'/>
+        <Form title='Parede 4'/>
+      </div>
+
+      <div className={styles.resultContainer}>
+
+      </div>
     </div>
   )
 }
