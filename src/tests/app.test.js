c0ef52acd941efa55c1e3d53aaf015627adb1790
Luciano Amâncio <madruga665@hotmail.com>
2021-11-04 12:00:18 -0300

    test: verifica se app tem titulo e subtitulo

---
+++ src/tests/app.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+
+describe('Verifica se a pagina tem title e subtitle', () => {
+  test('testa se a pagina tem o titulo', () => {
+    render(<App />);
+    const title = screen.getByText('Desafio Digital Republic');
+    expect(title).toBeInTheDocument();
+  });
+
+  test('testa se a pagina tem o subtitulo', () => {
+    render(<App />);
+    const title = screen.getByText('Calcule a quantidade de tinta para pintar uma sala');
+    expect(title).toBeInTheDocument();
+  });
+})
