640761bf2771b8cf0a18cdc1b360782a4a54dc06
Luciano Amâncio <madruga665@hotmail.com>
2021-11-04 17:08:04 -0300

    test: verifica se as mensagens de erro aparecem

--- src/tests/components/form.test.js
+++ src/tests/components/form.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
 import { render, screen } from '@testing-library/react';
 import App from '../../App';
+import userEvent from '@testing-library/user-event';
 
 describe('Verifica o componente Form', () => {
   test('Verifica se contem a parede com a numeração certa', () => {
@@ -48,5 +49,69 @@ describe('Verifica o componente Form', () => {
       name: 'Reiniciar'
     });
     expect(restartButton).toBeInTheDocument();
+  });
+});
+
+describe('Verifica se as mensagens de erro são renderizadas', () => {
+  test('Verifica se a mensagem de erro de Altura aparece, ao clicar no botão salvar sem preencher os campos', () => {
+    render(<App/>);
+    const saveButton = screen.getByRole('button', {
+      name: 'Salvar'
+    });
+    expect(saveButton).toBeInTheDocument();
+    userEvent.click(saveButton);
+
+    const minAndMaxHeightMessageError = screen.getByText('Altura mínima 1 metro e máxima 15 metros');
+    expect(minAndMaxHeightMessageError).toBeInTheDocument();
+  });
+
+  test('Verifica se a mensagem de erro de Largura aparece, ao clicar no botão salvar sem preencher o campo de Largura', () => {
+    render(<App/>);
+    const heightInput = screen.getByRole('spinbutton', {
+      name: /altura/i
+    });
+    expect(heightInput).toBeInTheDocument();
+    userEvent.type(heightInput, '5');
+
+    const saveButton = screen.getByRole('button', {
+      name: 'Salvar'
+    });
+    expect(saveButton).toBeInTheDocument();
+    userEvent.click(saveButton);
+
+    const minAndMaxWidthMessageError = screen.getByText('Largura mínima 1 metro e máxima 15 metros');
+    expect(minAndMaxWidthMessageError).toBeInTheDocument();
+  });
+
+  test('Verifica se a mensagem de erro de altura minima com porta aparece, ao clicar no botão salvar com altura minima menor que 2.20 e com 1 porta', () => {
+    render(<App/>);
+    const heightInput = screen.getByRole('spinbutton', {
+      name: /altura/i
+    });
+    expect(heightInput).toBeInTheDocument();
+    userEvent.type(heightInput, '2');
+
+    const widthInput = screen.getByRole('spinbutton', {
+      name: /largura/i
+    });
+    expect(widthInput).toBeInTheDocument();
+    userEvent.type(widthInput, '5');
+
+    const doorInput = screen.getByRole('spinbutton', {
+      name: /porta/i
+    });
+    expect(doorInput).toBeInTheDocument();
+    userEvent.type(widthInput, '1');
+
+    const saveButton = screen.getByRole('button', {
+      name: 'Salvar'
+    });
+    expect(saveButton).toBeInTheDocument();
+    userEvent.click(saveButton);
+
+    const { getByText } = render(
+      <span>Paredes com porta devem ter no minimo 2.20m de altura</span>
+    );
+    expect(getByText('Paredes com porta devem ter no minimo 2.20m de altura')).toBeInTheDocument();
   });
 });
